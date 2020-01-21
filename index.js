460474de87dc2c79b0c02d985a8548bf013acdb7
azatot2014 <azatot2014@gmail.com>
2020-01-21 18:00:14 +0200

    server created, some deps installed

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-console.log("Start");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send("started")
+});
+
+app.listen(PORT, () => {
+    console.log(`___SERVER LISTENING ON PORT ${PORT}___`)
+});
+
 
 
