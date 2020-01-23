2686a853a923464724029116f432f3a40d1f2c78
azatot2014 <azatot2014@gmail.com>
2020-01-23 16:31:04 +0200

    added something like todo-list

--- index.js
+++ index.js
@@ -1,10 +1,53 @@
 const express = require("express");
+// const hbs = require('hbs');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.set('view engine', 'hbs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+const arr = ["NodeJS", "Express", "Handlebars"];
+app.get('/', (req, res) => {
+    res.render("index", {
+        title: "Home",
+        link: "/create",
+        list: arr
+    })
+});
+app.get('/create', (req, res) => {
+    res.render('create');
+});
+app.post('/create', (req, res) => {
+    // res.render("create");
+    if(arr.includes(req.body.text)) {
+        res.send("Already exist this list item")
+    } else {
+        arr.push(req.body.text);
+        res.redirect('/')
+    }
+    console.log(req.body);
+});
+
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send("started")
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`___SERVER LISTENING ON PORT ${PORT}___`)
