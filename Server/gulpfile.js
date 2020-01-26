4ce290a9b7b0fcc365f40c92669aa96df1af5031
Denys <azatot2014@gmail.com>
2020-01-27 00:44:22 +0200

    added layout to back

---
+++ Server/gulpfile.js
@@ -0,0 +1,26 @@
+
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+// const browserSync = require('browser-sync').create();
+// const plumber = require('gulp-plumber');
+sass.compiler = require('node-sass');
+
+
+gulp.task('scss', () => {
+    return gulp.src('./dev/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
+            cascade: true
+        }))
+        .pipe(cssnano())
+        .pipe(gulp.dest('./public/stylesheets/'))
+});
+
+
+// works!
+gulp.task('default',gulp.series('scss'), () => {
+    gulp.watch('./dev/scss/**/*.scss', gulp.series('scss'));
+});
