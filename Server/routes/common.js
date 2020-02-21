00ebdbfcca34b64b00e4c362d3a933614c84f0a4
Denys <azatot2014@gmail.com>
2020-02-21 07:49:35 +0200

    started adding editing

--- Server/routes/common.js
+++ Server/routes/common.js
@@ -1,6 +1,9 @@
 const notAuthorised = (id, login, res, callback) => {
     if (!id && !login) {
-        res.redirect('/')
+        res.json({
+            resultCode: 102,
+            reason: "Not authorised, you must log in or register"
+        })
     } else {
         callback()
     }
--- Server/routes/post.js
+++ Server/routes/post.js
@@ -2,41 +2,70 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 const TurndownService = require('turndown');
-const { notAuthorised } = require('./common');
+const {notAuthorised} = require('./common');
+router.get('/:postName', async (req, res, next) => {
+    const url = req.params.postName.trim().replace(/ +(?= )/g, '');
+    const userId = req.session.userId;
+    const userLogin = req.session.userLogin;
 
+    if (!url) {
+        const err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    } else {
+        try {
+            const post = await db.Post.findOne({
+                url
+            }).populate('author');
+
+            if (!post) {
+                const err = new Error('Not Found');
+                err.status = 404;
+                next(err);
+            } else {
+                res.json({
+                    resultCode: 101,
+                    post: [post],
+                    user: {
+                        id: userId,
+                        login: userLogin
+                    }
+                });
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+});
 router.get('/add', (req, res) => {
-    const { userId: id, userLogin: login } = req.session;
+    const {userId: id, userLogin: login} = req.session;
     notAuthorised(id, login, res, () => {
-        res.json( {
-            user: { id, login },
+        res.json({
+            user: {id, login},
             renderPostAddPage: false
         })
     })
 });
 router.post('/add', async ({
-    body: {
-        postTitle: title,
-        postBody: body, 
-    },
-    session: {
-        userId,
-        userLogin
-    }, ...req }, res) => {
+                               body: {
+                                   postTitle: title,
+                                   postBody: body,
+                               },
+                               session: {
+                                   userId,
+                                   userLogin
+                               }, ...req
+                           }, res) => {
 
     title.trim().replace(/ +(?=)/g, '');
     const turndownService = new TurndownService();
     // const { userLogin: login } = req.session;
-    const fields = [];
-
-    !title && fields.push('post-title');
-    !body && fields.push('post-medium-editor');
 
     if (!title || !body) {
         res.json({
             resultCode: 102,
             type: 'error',
             message: 'All fields must be filled',
-            fields
         })
     } else {
         await db.Post.create({
@@ -51,4 +80,22 @@ router.post('/add', async ({
         })
     }
 });
+
+router.put('/:postName/edit', async (req, res, next) => {
+    const {userId: id, userLogin: login} = req.session;
+    const postName = req.params.postName.trim().replace(/ +(?= )/g, '');
+    notAuthorised(id, login, res, () => {
+        try {
+            res.json({
+                message: "rout /posts/:postName/edit WORKING GOOOOD)))",
+                receivedPost: postName,
+            })
+        } catch (e) {
+            console.log(e)
+        }
+
+    });
+});
+
+
 module.exports = router;
